9504005b7351882e3bb9f710342ec8681597ff91
Vincent-Caraga <vincentccaraga@gmail.com>
2025-07-22 14:46:18 +0800

    new commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,7 @@
 import book_1 from "./book_1.jpg";
+import book_2 from "./book_2.jpg";
 
 export const assets = {
   book_1,
+  book_2,
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,11 @@ const Header = () => {
     <div>
       <h1 className="title">TRENDING BOOKS</h1>
       <div className="image-container">
-        <img className="atomic" src={assets.book_1} alt="Atomic Habits" />
+        <img className="book" src={assets.book_1} alt="Atomic Habits" />
+
         <div className="book-description">
-          <h1 className="atomic-title">Atomic Habits</h1>
-          <h2 className="atomic-author">by: James Clear</h2>
+          <h1 className="book-title">Atomic Habits</h1>
+          <h2 className="book-author">by: James Clear</h2>
           <p className="description">
             "Atomic Habits" by James Clear is a personal development book that
             explains how tiny, consistent improvements in behavior can lead to
@@ -22,6 +23,15 @@ const Header = () => {
             improvements every day — like reading a page, doing one push-up, or
             choosing water over soda — you create a system of positive change
             that builds up over time.
+          </p>
+
+          <img className="book-beself" src={assets.book_2} alt="" />
+          <h1 className="book-self">Be Your Future Self Now</h1>
+          <h2 className="book-author">Dr. Benjamin Hardy</h2>
+          <p className="description">
+            Be Your Future Self Now is a transformative self-help book that
+            encourages readers to stop being defined by their past and instead
+            be pulled forward by a vivid, compelling vision of their future.
           </p>
         </div>
       </div>
